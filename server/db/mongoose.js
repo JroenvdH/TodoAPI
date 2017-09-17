cc6b5294af3086477fa5a77d7992529063ed2fc5
Klaas van der Hulst <Klaas van der Hulst>
2017-09-17 12:47:14 +0200

    debug db connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 let db = {
-	mlab: 'mongodb://KekoLdii:jerHh9856@ds141274.mlab.com:41274/todo_api',
+	mlab: 'mongodb://keko:testtest@ds141274.mlab.com:41274/todo_api',
 	localhost: 'mongodb://localhost:27017/TodoApp'
 }
 
